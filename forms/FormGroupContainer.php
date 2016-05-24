fd9def91ae859d698886e986756ed362fff884b0
MacMini2 <djsucho@gmail.com>
2016-05-24 14:45:36 +0200

    ADD empty formGroupContainer

---
+++ forms/FormGroupContainer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wame\DynamicObject\Forms;
+
+use Wame\DynamicObject\Forms\BaseFormContainer;
+
+interface IFormGroupContainerFactory
+{
+	/** @return FormGroupContainer */
+	function create();
+}
+
+
+class FormGroupContainer extends BaseFormContainer
+{
+	/*@var string */
+	private $title;
+	
+	
+	public function __construct($title = null) 
+	{
+		parent::__construct();
+		
+		$this->title = $title;
+	}
+	
+	
+	protected function configure() 
+	{		
+		$form = $this->getForm();
+
+        $form->addGroup($this->title);
+    }
+
+	
+	public function render() {
+		
+	}
+
+}
