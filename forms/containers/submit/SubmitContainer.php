1c2f9d78c2cb1d3d56a462a8cbbbd6feb32ee5c4
Stanislav Červeňák <stano@wame.sk>
2016-10-19 09:50:01 +0200

    EDIT submit form container

--- forms/containers/submit/SubmitContainer.php
+++ forms/containers/submit/SubmitContainer.php
@@ -5,24 +5,27 @@ namespace Wame\DynamicObject\Forms\Containers;
 use Wame\DynamicObject\Registers\Types\IBaseContainer;
 use Wame\DynamicObject\Forms\Groups\TransparentGroup;
 
+
 interface ISubmitContainerFactory extends IBaseContainer
 {
-	/** @return SubmitContainer */
-	public function create();
+    /** @return SubmitContainer */
+    public function create();
 }
+
 
 class SubmitContainer extends BaseContainer
 {
     /** {@inheritDoc} */
-    public function configure() 
-	{
+    public function configure()
+    {
         $group = new TransparentGroup;
         $group->setTag('div');
-        
+
         $this->getForm()->addBaseGroup($group);
-        
-		$this->addSubmit('submit', _('Submit'))
-            ->setAttribute('data-submit', 'form');
+
+        $this->addSubmit('submit', _('Submit'))
+                ->setAttribute('data-submit', 'form');
     }
+
 
 }
