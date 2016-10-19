496768ac730bc56580cb60dc3d100870b18de1bd
Stanislav Červeňák <stano@wame.sk>
2016-10-19 10:07:39 +0200

    EDIT basicGroup width

--- forms/groups/basic/BasicGroup.php
+++ forms/groups/basic/BasicGroup.php
@@ -8,25 +8,18 @@ use Wame\DynamicObject\Forms\Groups\BaseGroup;
 
 interface IBasicGroupFactory extends IBaseContainer
 {
-	/** @return BasicGroup */
-	function create();
+    /** @return BasicGroup */
+    function create();
 }
 
 
 class BasicGroup extends BaseGroup
 {
-    public function __construct()
-    {
-        parent::__construct();
-        
-        $this->setWidth('col-sm-6');
-    }
-    
-    
     /** {@inheritDoc} */
     public function getText()
     {
         return _('Basic');
     }
+
 
 }
