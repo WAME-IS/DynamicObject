32969265ce105984fbd922e3e81a9ce0545e670f
Stanislav Červeňák <stano@wame.sk>
2016-11-30 09:26:40 +0100

    EDIT descriptionFormContainer - add group

--- forms/containers/descriptionEditor/DescriptionEditorContainer.php
+++ forms/containers/descriptionEditor/DescriptionEditorContainer.php
@@ -4,6 +4,8 @@ namespace Wame\DynamicObject\Forms\Containers;
 
 use Wame\DynamicObject\Registers\Types\IBaseContainer;
 use Wame\DynamicObject\Forms\Containers\DescriptionContainer;
+use Wame\DynamicObject\Forms\Groups\DescriptionGroup;
+
 
 interface IDescriptionEditorContainerFactory extends IBaseContainer
 {
@@ -11,7 +13,14 @@ interface IDescriptionEditorContainerFactory extends IBaseContainer
     public function create();
 }
 
+
 class DescriptionEditorContainer extends DescriptionContainer
 {
-    
+    /** {@inheritDoc} */
+    public function configure()
+    {
+        $this->getForm()->addBaseGroup(new DescriptionGroup, 'DescriptionGroup');
+        $this->addTextArea('description', _('Description'));
+    }
+
 }
