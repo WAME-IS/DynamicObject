ffc94cc21b3deae127da03f97caa8a98e50d93fd
Stanislav Červeňák <stano@wame.sk>
2017-12-18 08:01:01 +0100

    CREATE DataScrapper

--- forms/containers/url/UrlContainer.php
+++ forms/containers/url/UrlContainer.php
@@ -4,11 +4,13 @@ namespace Wame\DynamicObject\Forms\Containers;
 
 use Wame\DynamicObject\Registers\Types\IBaseContainer;
 
+
 interface IUrlContainerFactory extends IBaseContainer
 {
 	/** @return UrlContainer */
 	public function create();
 }
+
 
 class UrlContainer extends BaseContainer
 {
@@ -18,17 +20,20 @@ class UrlContainer extends BaseContainer
 		$this->addText('url', _('Url'));
     }
 
+
     /** {@inheritDoc} */
 	public function setDefaultValues($entity)
 	{
         $this['url']->setDefaultValue($entity->getUrl());
 	}
 
+
     /** {@inheritDoc} */
     public function create($form, $values)
     {
         $form->getEntity()->setUrl($values['url']);
     }
+
 
     /** {@inheritDoc} */
     public function update($form, $values)
